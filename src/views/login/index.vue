f65a94874481306792a3e81d044b7674fa1a2662
Zhou-ly <1959649788@qq.com>
2020-11-25 22:33:12 +0800

    form valid

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,11 +9,11 @@
       <div class="login-head">
         <div class="logo"></div>
       </div>
-      <el-form class="login-form" ref="form" :model="user">
-        <el-form-item>
+      <el-form class="login-form" ref="login-form" :model="user" :rules="formRules">
+        <el-form-item prop="mobile">
           <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
         </el-form-item>
         <el-form-item>
@@ -45,7 +45,18 @@ export default {
         code: ''
       },
       checked: false,
-      loginLoading: false
+      loginLoading: false,
+      formRules: { // 表单验证规则配置
+        // 要验证的数据名称：规则列表[]
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '请输入正确的号码格式', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空', trigger: 'change' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码格式' }
+        ]
+      }
     }
   },
   computed: {},
@@ -54,16 +65,28 @@ export default {
   mounted () {},
   methods: {
     onLogin () {
-      // 获取表单数据（根据接口要求绑定数据）
-      const user = this.user
+      let isPassed = true
       // 表单验证
-      // 验证通过，提交登录
+      // validate 方法是异步的
+      this.$refs['login-form'].validate(valid => {
+        // 如果表单验证失败，停止请求提交
+        if (!valid) {
+          isPassed = false
+        }
+      })
+      // 验证通过，请求登录
+      if (isPassed) {
+        this.login()
+      }
+    },
+
+    login () {
       this.loginLoading = true
       request({
         method: 'POST',
         url: '/mp/v1_0/authorizations',
         // data 用来设置 POST 请求体
-        data: user
+        data: this.data
       }).then(res => {
         console.log(res)
         // 登录成功
